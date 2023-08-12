6f34374de6a51a5f0ef262c84126b3ffb1dcad54
rungene <rungenesamuel@gmail.com>
2023-08-12 18:46:04 +0000

    Task: 7. Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- creates a stored procedure ComputeAverageScoreForUser
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE average_score FLOAT;
+  SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
+  UPDATE users SET average_score = average_score WHERE id = user_id;
+  IF ROW_COUNT() = 0 THEN
+    INSERT INTO users (id, average_score) VALUES (user_id, average_score);
+  END IF;
+END;
+//
+
+DELIMITER ;
