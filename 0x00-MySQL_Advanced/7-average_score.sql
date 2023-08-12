0f5b6e366f731b089e1791f92196924611c3ce85
rungene <rungenesamuel@gmail.com>
2023-08-12 19:07:22 +0000

    Corrections

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,8 +6,8 @@ BEGIN
   DECLARE average_score FLOAT;
   SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
   UPDATE users SET average_score = average_score WHERE id = user_id;
-  IF ROW_COUNT() = 0 THEN
-    INSERT INTO users (id, average_score) VALUES (user_id, average_score);
-  END IF;
+--  IF ROW_COUNT() = 0 THEN
+--    INSERT INTO users (id, average_score) VALUES (user_id, average_score);
+--  END IF;
 END //
 DELIMITER ; //
