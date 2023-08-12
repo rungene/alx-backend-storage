bcc3f0a9166a4e5f2ff17f64764712c112d76ade
rungene <rungenesamuel@gmail.com>
2023-08-12 19:14:33 +0000

    Corrections

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,11 +3,11 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  DECLARE average_score FLOAT;
-  SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
-  UPDATE users SET average_score = average_score WHERE id = user_id;
---  IF ROW_COUNT() = 0 THEN
---    INSERT INTO users (id, average_score) VALUES (user_id, average_score);
---  END IF;
+  DECLARE avg_score FLOAT;
+  SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+  UPDATE users SET average_score = avg_score WHERE id = user_id;
+  IF ROW_COUNT() = 0 THEN
+    INSERT INTO users (id, average_score) VALUES (user_id, average_score);
+  END IF;
 END //
 DELIMITER ; //
